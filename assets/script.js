110a5dca9732ae1c14a908890eced857ea38a080
razaquat.ahmed <razaquat@hotmail.co.uk>
2022-06-03 09:57:16 +0100

    working- site- coding-completed

--- assets/script.js
+++ assets/script.js
@@ -2,81 +2,93 @@ $(document).ready(function () {
   //Global Varibles - to select today date - store calander events - track calander current time
   const currentDateEl = $("header #currentDay");
   let calEvents = {};
-  let renderHour = moment();
+  let hourRendered = moment();
 
-  //on pageload - get current time to place on header  - create time blocks  - track hour to change blocks every hour
-  loadcalendar();
-  initCalendar();
-  trackBlock();
-
-  // render the calendar on the page  - only to start buliding from 9 o clock till 5 - ( appended class and save button to each block) - it statement to check teh current hour and ammend acordingly
-  function renderTimeblock(today, calEvents) {
-    let currentHour = moment(today).hour(9);
+  // render the calendar on the page from 9 0 clocnbk
+  function renderCalendar(today, calEvents) {
+    let rowHr = moment(today).hour(9);
     const calendar = $("div.container");
     calendar.empty();
 
+    // for loop to make rows for hours 9 to 5
     for (let i = 1; i < 10; i++) {
       const row = $("<div>").addClass("row");
 
-      let hourClass = "";
-      if (today.isBefore(currentHour, "hour")) {
-        hourClass = "future";
-      } else if (today.isAfter(currentHour, "hour")) {
-        hourClass = "past";
+      //set colors for time blocks for past, present and future
+      let classOfHour = "";
+      if (today.isBefore(rowHr, "hour")) {
+        classOfHour = "future";
+      } else if (today.isAfter(rowHr, "hour")) {
+        classOfHour = "past";
       } else {
-        hourClass = "present";
+        classOfHour = "present";
       }
 
       calendar.append(row);
 
-      row.append(
-        $("<div>").addClass("col-2 hour").text(currentHour.format("h A"))
-      );
+      row.append($("<div>").addClass("col-2 hour").text(rowHr.format("h A")));
 
-      let timeBlock = currentHour.format("hA");
+      let timeBlock = rowHr.format("hA"); // keys for data in calEvents object to populate textarea
       row.append(
         $("<textarea>")
-          .addClass(`col-8 ${hourClass}`)
+          .addClass(`col-8 ${classOfHour}`)
           .text(calEvents[timeBlock])
       );
+      //adding  save button column to row
       row.append(
         $("<button>")
           .addClass("col-2 saveBtn")
           .html("<i class='fas fa-save'></i>")
           .attr("aria-label", "Save")
-          .attr("id", currentHour.format("hA"))
+          .attr("id", rowHr.format("hA"))
       );
-      currentHour.add(1, "hour");
-      renderHour = moment();
+
+      rowHr.add(1, "hour");
+
+      hourRendered = moment();
     }
   }
 
   // start funtion ammend heder to read current time and date and using date to render timeBLock
   function initCalendar() {
-    const today = moment();
-    currentDateEl.text(today.format("LLLL"));
-    renderTimeblock(today, calEvents);
+    const today = moment(); // set today's date
+    currentDateEl.text(today.format("LL"));
+    renderCalendar(today, calEvents);
   }
 
-  /// on page load funtion to see if ther is any items on local stoarge then display the events
-  function loadcalendar() {
+  function loadCal() {
     const storedCal = JSON.parse(localStorage.getItem("calEvents"));
     if (storedCal) {
       calEvents = storedCal;
     }
   }
 
-  function trackBlock() {
-    const checkHourInterval = setInterval(function () {
-      if (moment().isAfter(renderHour, "minute")) {
-        initCalendar();
-      }
-    }, 60000);
-  }
+  //on pageload - get current time to place on header  - create time blocks  - track hour to change blocks every hour
 
-  // storeing evenst item to loacalstorage as JSON items
-  function storeCal() {
-    localStorage.setItem("calEvents", JSON.stringify(calEvents));
-  }
+  loadCal();
+  initCalendar();
+  hourTracker();
 
+  /// on page load funtion to see if ther is any items on local stoarge then display the events
+  function loadcalendar() {
+    function hourTracker() {
+      const checkHourInterval = setInterval(function () {
+        if (moment().isAfter(hourRendered, "minute")) {
+          initCalendar();
+        }
+      }, 60000);
+    }
+
+    //adds items local storage
+    function storeCal() {
+      localStorage.setItem("calEvents", JSON.stringify(calEvents));
+    }
+
+    // storeing evenst item to loacalstorage
+    $(document).on("click", "button.saveBtn", function (event) {
+      let calDesc = event.currentTarget.parentElement.children[1].value;
+      calEvents[event.currentTarget.id] = calDesc;
+      storeCal();
+    });
+  }
 });
